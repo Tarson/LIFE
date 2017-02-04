f46aadc5af35c0d6aff8cde1c9376e9d21fc367f
m <sipnot2mail.ru>
2017-02-04 23:22:11 +0300

    1

---
+++ src/Lcage.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+/**
+ * Created by m on 04.02.2017.
+ */
+public class Lcage {
+
+
+    JButton b;
+    int x;
+    int y;
+    boolean aLive;
+
+    // creation of CELL with association with button and coordinates x and y
+    Lcage(JButton b, int x, int y){
+        this.b=b;
+        this.x=x;
+        this.y=y;
+        aLive=true;
+
+        System.out.println(b.getText()+" "+x+" "+y);
+
+  }
+
+  // method to put CELL object to list array 
+
+}
